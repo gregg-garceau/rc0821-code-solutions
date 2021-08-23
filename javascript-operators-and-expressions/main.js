a85780f8867ed8b66fb4705e00ee25f70678fb70
Greggory Garceau <ggarceau212@gmail.com>
2021-08-23 14:27:21 -0400

    Completed javascript operators and expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,53 @@
+var width = 7;
+var height = 8;
+
+var area = width * height;
+
+console.log('Area: ', area);
+console.log('Type of area: ', typeof area);
+
+var bill = 1000;
+var payment = 200;
+
+var change = bill - payment;
+
+console.log('Change: ', change);
+console.log('Type of change: ', typeof change);
+
+var quizzes = 80;
+var midterm = 90;
+var final = 95;
+
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('Grade: ', grade);
+console.log('Type of grade: ', typeof grade);
+
+var firstName = 'Gregg';
+var lastName = 'Garceau';
+
+var fullName = firstName + ' ' + lastName;
+
+console.log('Full name: ', fullName);
+console.log('Type of full name: ', typeof fullName);
+
+var pH = 10;
+
+var isAcidic = pH < 7;
+
+console.log('Is acidic?: ', isAcidic);
+console.log('Type of is acidic: ', typeof isAcidic);
+
+var headCount = 300;
+
+var isSparta = headCount === 300;
+
+console.log('Is this sparta?: ', isSparta);
+console.log('Type of is sparta: ', typeof isSparta);
+
+var motto = fullName;
+
+motto = motto + ' is the GOAT';
+
+console.log('Motto: ', motto);
+console.log('Type of motto: ', typeof motto);
