df0de00df3789893b84e98d9cd441ad62a6a2bec
Greggory Garceau <ggarceau212@gmail.com>
2021-08-24 17:43:43 -0400

    Completed javascript functions practice

---
+++ javascript-functions-practice/get-average-of-three-numbers.js
@@ -0,0 +1,5 @@
+/* exported getAverageOfThreeNumbers */
+function getAverageOfThreeNumbers(x, y, z) {
+  var averageThree = (x + y + z) / 3;
+  return averageThree;
+}
---
+++ javascript-functions-practice/get-average-of-two-numbers.js
@@ -0,0 +1,5 @@
+/* exported getAverageOfTwoNumbers */
+function getAverageOfTwoNumbers(x, y) {
+  var averageTwo = (x + y) / 2;
+  return averageTwo;
+}
