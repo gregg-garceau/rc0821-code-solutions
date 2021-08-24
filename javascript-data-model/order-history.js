997fbfd68ac71d2156402c0c3510d856d1cea56d
Greggory Garceau <ggarceau212@gmail.com>
2021-08-24 13:47:58 -0400

    Completed javascript data model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,61 @@
+var orderHistory = [
+  {
+    orderDate: 'August 4, 2020',
+    total: 34.00,
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    deliveryDate: 'Aug 8, 2020',
+    items: [
+      {
+        book: 'Javascript for impatient programmers'
+      }
+    ],
+    returnWindow: 'Aug 19, 2020'
+  },
+
+  {
+    orderDate: 'July 19, 2020',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    deliveryDate: 'Jul 20, 2020',
+    items: [
+      {
+        book: 'The Timeless Way of Building'
+      }
+    ],
+    returnWindow: 'Aug 19, 2020'
+  },
+
+  {
+    orderDate: 'July 4, 2020',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    deliveryDate: 'Jul 7 2020',
+    items: [
+      {
+        electronics: 'Gamecuber Controller Adapter. Super Smash Bros Switch Gamecube ' +
+        'Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter'
+      }
+    ],
+    returnWindow: 'Aug 5, 2020'
+  },
+
+  {
+    orderDate: 'July 3, 2020',
+    total: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    deliveryDate: 'Jul 5, 2020',
+    items: [
+      {
+        electronics: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        book: 'The Art of Sql'
+      }
+    ],
+    returnWindow: 'Aug 4, 2020'
+  }
+];
+
+console.log(orderHistory[3].items[0].electronics);
