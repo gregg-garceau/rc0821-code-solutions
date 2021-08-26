5be838af81a4bcae118a137f16f5a9042d9f96d3
Greggory Garceau <ggarceau212@gmail.com>
2021-08-26 16:16:10 -0400

    redone js loops practice

--- javascript-loops-practice/countdown.js
+++ javascript-loops-practice/countdown.js
@@ -1,10 +1,10 @@
 /* exported countdown */
 function countdown(number) {
   var numbers = [];
-  var i = number;
-  while (i >= 0) {
-    numbers.push(i);
-    i -= 1;
+  var c = number;
+  while (c >= 0) {
+    numbers.push(c);
+    c -= 1;
   }
   return numbers;
 }
