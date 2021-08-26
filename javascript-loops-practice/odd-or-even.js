00830482a44ca8f85499f5b5f137efe2343462fd
Greggory Garceau <ggarceau212@gmail.com>
2021-08-26 15:58:43 -0400

    Completed javascript loops practice

---
+++ javascript-loops-practice/odd-or-even.js
@@ -0,0 +1,13 @@
+/* exported oddOrEven */
+function oddOrEven(numbers) {
+  var oddEven = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      oddEven.push('even');
+    } else {
+      oddEven.push('odd');
+    }
+  }
+  return oddEven;
+}
