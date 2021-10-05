77ac87aaf4ee7de33ad11b49225d6a9c1c752085
Gregg Garceau <ggarceau212@gmail.com>
2021-10-05 23:41:08 +0000

    completed node-module-system exercise

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('exports typeof: ', typeof exports);
+console.log('value exports: ', exports);
+
+console.log('require typeof: ', typeof require);
+console.log('value require: ', require);
+
+console.log('module typeof: ', typeof module);
+console.log('value module: ', module);
+
+console.log('__filename typeof: ', typeof __filename);
+console.log('value __filename: ', __filename);
+
+console.log('__dirname typeof: ', typeof __dirname);
+console.log('value __dirname: ', __dirname);
